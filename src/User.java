7ad4223148fc154f07fd1fd2bf0f7136c1e0e7eb
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-14 14:29:33 -0300

    User info validation

--- src/User.java
+++ src/User.java
@@ -1,21 +1,43 @@
 import java.io.Serializable;
+import java.util.List;
 
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-    String nome;
+    String name;
     String password;
+    
 
-    public User(String nome, String password) {
-        this.nome = nome;
+    public User(String name, String password) {
+        this.name = name;
         this.password = password;
     }
 
+    public User(){
+
+    }
+
         // Getters para acessar os valores após desserialização
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
     public String getPassword() {
         return password;
     }
+
+    List<User> userList;
+
+    public boolean userValidation(String name, String password) {
+        
+
+        for(User users: userList) {
+            if(users.getName().equals(name) && users.getPassword().equals(name)){
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
 }
+
